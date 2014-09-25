01927948faa1f2077d9c8b187ad58877cf1f9b4b
Tyler Bell <tyler@tylerwbell.com>
2014-09-25 12:59:44 -0400

    change router to angular ui router. add account route

--- web-client/app/script/app.js
+++ web-client/app/script/app.js
@@ -1,15 +1,23 @@
-var ThriftShopApp = angular.module('ThriftShopApp', ['ngRoute']);
+var ThriftShopApp = angular.module('ThriftShopApp', ['ui.router']);
 
-ThriftShopApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/categories', {
-        templateUrl: 'partials/categories.html'
-      }).
-      when('/signin', {
-		  templateUrl: 'partials/signin.html'
-      }).
-      otherwise({
-        redirectTo: '/signin'
-      });
-  }]);
+ThriftShopApp.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/signin");
+	
+	$stateProvider
+	.state('signin', {
+		url: '/signin',
+		templateUrl: 'partials/signin.html'
+	})
+	.state('account', {
+		url: '/account',
+		templateUrl: 'partials/account.html'
+	})
+	.state('main', {
+		url: '/main',
+		templateUrl: 'partials/main.html'
+	})
+	.state('main.categories', {
+		url: "/categories",
+		templateUrl: 'partials/main.categories.html',
+	});
+});
